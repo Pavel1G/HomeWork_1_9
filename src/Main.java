65964c3e78aeeced4622af1c94e3fa23473aeaf9
Pavel Golubev <pavelgolubew@gmail.com>
2022-08-24 23:33:03 +0300

    Homework done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Author kirBulyschev = new Author("Kir", "Bulychev");
+        Author georgeOrwell = new Author("George", "Orwell");
+
+        Book bookAlice = new Book("Alice: The Girl from Earth", kirBulyschev, 2002);
+        Book book1984 = new Book("1984", georgeOrwell, 1949);
+//        System.out.println("bookAlice.getYearWriting() = " + bookAlice.getYearWriting());
+        bookAlice.setYearWriting(1985);
+//        System.out.println("bookAlice.getYearWriting() = " + bookAlice.getYearWriting());
+
     }
 }
